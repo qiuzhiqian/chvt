9dc73fbf1120e9efb548a14e7c6723c81b7e1d39
xiamengliang <xiamengliang@uniontech.com>
2021-07-20 20:32:45 +0800

    添加tty序号设置

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/qiuzhiqian/chvt"
 )
 
 func main() {
+	var num int = -1
 	log.Println("chvt")
-	err := chvt.Chvt(9)
+	flag.IntVar(&num, "num", 0, "change active tty to ttyN")
+	flag.Parse()
+
+	if num <= 0 {
+		flag.Usage()
+		return
+	}
+	log.Println("num:", num)
+	err := chvt.Chvt(uint(num))
 	if err != nil {
 		log.Println(err)
 	}
